56198ae2e9a19ae5d333a66b50537f50bbd4a048
Max Huang <maxhuang@apple.com>
2019-03-07 21:15:45 +0800

    Add more code and unit test.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 /*
  A Depth of Field calculator written in Node/Javascript.
+ https://stackoverflow.com/questions/22097603/unit-testing-of-private-functions-with-mocha-and-node-js
  */
-
 "use strict";
 
-exports.UNITS = {
-    CM: 0.0100,
-    METER: 1.0000,
-    FEET: 0.3048,
-    INCH: 0.0254 
+// All units based on millimeter.
+const UNITS = {
+  CM: 10.0,
+  METER: 1000.0,
+  FEET: 304.8,
+  INCH: 25.4
 };
 
-exports.calc = function(coc, focal, aperture, distance, unit) {
-    return 1.00;
+/*
+ Use the same millimeter unit to unify all the input parameters.
+ */
+function unifyUnit(input, unit) {
+  input.distance = input.distance * unit;
+  return input;
+}
+
+function calc(coc, focal, aperture, distance, unit = UNITS.METER) {
+  input = unifyUnit({
+      coc: coc,
+      focal: focal,
+      aperture: aperture,
+      distance: distance
+  }, unit);
+  //Calculate hyper focal and set it to the result bean.
+  hyperFocal = (focal * focal) / (aperture * coc) + focal;
 
 
 
-};
 
+}
+
+
+
+
+exports.UNITS = UNITS;
+
+exports.calc = calc;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,33 @@
+/*
+  https://stackoverflow.com/a/31462773/3115617
+ */
+
 const assert = require('assert');
 const expect = require("chai").expect;
-const dofc = require('../src/index');
+const rewire = require('rewire');
+const dofc = rewire('../src/index');
 
-describe("Demo test", function() {
+describe("dofc test", function() {
   it("get UNITS length", function() {
   let unitsLength = Object.keys(dofc.UNITS).length;
   expect(unitsLength).to.equal(4);
+  });
+
+  it("test unifyUnit", function() {
+  let unifyUnit = dofc.__get__('unifyUnit');
+  input = {
+      coc: 0.1,
+      focal: 50,
+      aperture: 0.5,
+      distance: 10
+  };
+  output = unifyUnit(input, dofc.UNITS.METER);
+  console.log(output);
+  expect(Object.keys(output).length).to.equal(4);
+  expect(output.coc).to.equal(0.1);
+  expect(output.focal).to.equal(50);
+  expect(output.aperture).to.equal(0.5);
+  expect(output.distance).to.equal(10000);
+
   });
 });
