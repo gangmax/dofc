3961e7a1762aae37a366e9713fc46f1de6f7da83
Max Huang <maxhuang@apple.com>
2019-03-08 12:18:00 +0800

    Add the implemention code(not fully testing yet).

--- src/index.js
+++ src/index.js
@@ -12,32 +12,87 @@ const UNITS = {
   INCH: 25.4
 };
 
+// //Set infinity distance as 10km.
+const INFINITY_DISTANCE = 10.0 * 1000 * 1000;
+
 /*
  Use the same millimeter unit to unify all the input parameters.
  */
-function unifyUnit(input, unit) {
+function unifyInput(input, unit) {
   input.distance = input.distance * unit;
   return input;
 }
 
+/*
+ Unify all the output properties back to passed-in unit.
+ */
+function unifyOutput(output, unit) {
+  Object.keys(output).forEach(key => output[key] = output[key] / unit);
+  return output;
+}
+
+/*
+ All parameters in "unifiedInput" are based on millimeter or without unit:
+   coc, focal, aperture, distance.
+ The output is a object contains the 7 following numbers:
+   Calculate: hyperFocal, nearLimit, farLimit, totalDepth, frontPercent,
+   hehindPercent, frontDepth, behindDepth.
+ */
+function calculate(unifiedInput) {
+  const coc = unifiedInput.coc;
+  const focal = unifiedInput.focal;
+  const aperture = unifiedInput.aperture;
+  const distance = unifiedInput.distance;
+  // Calculate: hyperFocal, nearLimit, farLimit, totalDepth, frontPercent,
+  // hehindPercent, frontDepth, behindDepth.
+  const hyperFocal = (focal * focal) / (aperture * coc) + focal;
+  const nearLimit = ((hyperFocal - focal) * distance) / 
+    (hyperFocal + distance - (2 * focal));
+  //Prevent 'divide by zero' when calculating far distance.
+  let farLimit = 1.0;
+  if ((hyperFocal - distance) <= 0.00001) {
+    //Set infinity at 10000m.
+    farLimit = INFINITY_DISTANCE;
+  } else {
+    farLimit = ((hyperFocal - focal) * distance) / (hyperFocal - distance);
+  }
+  let totalDepth = farLimit - nearLimit;
+  const frontPercent = (distance - nearLimit) / (farLimit - nearLimit) * 100.0;
+  const hehindPercent = (farLimit - distance) / (farLimit - nearLimit) * 100.0;
+  let frontDepth = 1.0;
+  let behindDepth = 1.0;
+  if (farLimit < INFINITY_DISTANCE) {
+    frontDepth = distance - nearLimit;
+    behindDepth = farLimit - distance;
+  } else {
+    farLimit = Infinity;
+    totalDepth = Infinity;
+    frontDepth = distance - nearLimit;
+    behindDepth = Infinity;
+  }
+  return {
+    hyperFocal,
+    nearLimit,
+    farLimit,
+    totalDepth,
+    frontPercent,
+    hehindPercent,
+    frontDepth,
+    behindDepth
+  }
+}
+
 function calc(coc, focal, aperture, distance, unit = UNITS.METER) {
-  input = unifyUnit({
+  let unifiedInput = unifyInput({
       coc: coc,
       focal: focal,
       aperture: aperture,
       distance: distance
   }, unit);
-  //Calculate hyper focal and set it to the result bean.
-  hyperFocal = (focal * focal) / (aperture * coc) + focal;
-
-
-
-
+  return unifyOutput(calculate(unifiedInput));
 }
 
-
-
-
-exports.UNITS = UNITS;
-
-exports.calc = calc;
+module.exports = {
+  UNITS,
+  calc
+}
--- test/test.js
+++ test/test.js
@@ -9,25 +9,49 @@ const dofc = rewire('../src/index');
 
 describe("dofc test", function() {
   it("get UNITS length", function() {
-  let unitsLength = Object.keys(dofc.UNITS).length;
-  expect(unitsLength).to.equal(4);
+    let unitsLength = Object.keys(dofc.UNITS).length;
+    expect(unitsLength).to.equal(4);
   });
 
-  it("test unifyUnit", function() {
-  let unifyUnit = dofc.__get__('unifyUnit');
-  input = {
+  it("test unifyInput", function() {
+    const unifyInput = dofc.__get__('unifyInput');
+    const input = {
       coc: 0.1,
       focal: 50,
       aperture: 0.5,
       distance: 10
-  };
-  output = unifyUnit(input, dofc.UNITS.METER);
-  console.log(output);
-  expect(Object.keys(output).length).to.equal(4);
-  expect(output.coc).to.equal(0.1);
-  expect(output.focal).to.equal(50);
-  expect(output.aperture).to.equal(0.5);
-  expect(output.distance).to.equal(10000);
+    };
+    const result = unifyInput(input, dofc.UNITS.METER);
+    console.log(result);
+    expect(Object.keys(result).length).to.equal(4);
+    expect(result.coc).to.equal(0.1);
+    expect(result.focal).to.equal(50);
+    expect(result.aperture).to.equal(0.5);
+    expect(result.distance).to.equal(10000);
+  });
 
+  it("test unifyOutput", function() {
+    const unifyOutput = dofc.__get__('unifyOutput');
+    const output = {
+      hyperFocal: 1000.0,
+      nearLimit: 100.0,
+      farLimit: 1300.0,
+      totalDepth: 1400.0,
+      frontPercent: 10.0,
+      hehindPercent: 90.0,
+      frontDepth: 5.0,
+      behindDepth: 10.0
+    };
+    const result = unifyOutput(output, dofc.UNITS.METER);
+    console.log(result);
+    expect(Object.keys(result).length).to.equal(8);
+    expect(result.hyperFocal).to.equal(1);
+    expect(result.nearLimit).to.equal(0.1);
+    expect(result.farLimit).to.equal(1.3);
+    expect(result.totalDepth).to.equal(1.4);
+    expect(result.frontPercent).to.equal(0.01);
+    expect(result.hehindPercent).to.equal(0.09);
+    expect(result.frontDepth).to.equal(0.005);
+    expect(result.behindDepth).to.equal(0.01);
   });
 });
